84abb7ba55b4d871994a490ad99e2a15ec4a41fe
kotodorii <gencoerden@gmail.com>
2024-09-13 00:46:43 -0400

    Implemented Task 2 (Reduce.java).

---
+++ src/Reduce.java
@@ -0,0 +1,20 @@
+public class Reduce {
+    public static void main(String[] args) {
+        System.out.println(reduceInt(2));
+    }
+    public static int reduceInt(int n) {
+        int count = 0;
+        while (n != 0) {
+            boolean isEven = n % 2 == 0;
+            boolean isOdd = !isEven;
+            if (isEven) {
+                n /= 2;
+            }
+            if (isOdd) {
+                n -= 1;
+            }
+            count ++;
+        }
+        return count;
+    }
+}
