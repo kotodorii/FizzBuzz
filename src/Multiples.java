33a40422784bfd418d5fcc03d9ff032adbd10ecb
kotodorii <gencoerden@gmail.com>
2024-09-12 23:43:55 -0400

    Task 1 implementation

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class Multiples {
-}
+    public static void main(String[] args) {
+        multipleCount(10);
+    }
+    public static void multipleCount(int i){
+        int count = 0;
+        while (i < 1000) {
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+            if (divisibleBy3 || divisibleBy5) {
+                count++;
+            }
+            i++;
+        }
+        System.out.println(count);
+    }}
